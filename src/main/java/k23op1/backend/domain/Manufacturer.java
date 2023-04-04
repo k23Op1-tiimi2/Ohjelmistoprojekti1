cc58f889e1bf392b0dfc28d39c91703c57747499
samuelmuittari <muittaris@gmail.com>
2023-04-04 15:17:20 +0300

    H2 tietokanta lisätty

--- src/main/java/k23op1/backend/domain/Manufacturer.java
+++ src/main/java/k23op1/backend/domain/Manufacturer.java
@@ -1,15 +1,14 @@
 package k23op1.backend.domain;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.CascadeType;
+// Kommentoin noi importit väliaikasesti pois, kun vscode ilmoitti ettei niitä käytetä ja tuli rumia keltaisia merkintöjä :(
+// import java.util.List;
+// import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+// import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+ // import jakarta.persistence.OneToMany;
 
 @Entity
 public class Manufacturer {
