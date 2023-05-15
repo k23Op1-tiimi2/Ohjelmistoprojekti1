b729c07dde52cf334f224517f813a3783af12dd2
Joonas Montonen <joonas.montonen@outlook.com>
2023-05-15 16:37:34 +0300

    Valmistajan nimeä voi muokata

--- src/main/java/k23op1/backend/web/ManufacturerController.java
+++ src/main/java/k23op1/backend/web/ManufacturerController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.validation.Valid;
 import k23op1.backend.domain.Manufacturer;
 import k23op1.backend.domain.ManufacturerRepository;
 
@@ -25,6 +30,16 @@ public class ManufacturerController {
         return "manufacturerlist";
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // valmistajan lisääminen
     @RequestMapping(value = "/addmanufacturer")
     public String createManufacturer(Model model) {
@@ -39,9 +54,50 @@ public class ManufacturerController {
         return "redirect:manufacturerlist";
     }
 
+    
+    
+    @GetMapping("/editmanufacturer/{manufacturerid}")
+public String showEditForm(@PathVariable("manufacturerid") Long manufacturerId, Model model) {
+    Manufacturer manufacturer = manufacturerRepository.findById(manufacturerId)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid manufacturer ID: " + manufacturerId));
+    model.addAttribute("manufacturer", manufacturer);
+    return "editmanufacturer";
+}
+
+@PostMapping("/editmanufacturer/{manufacturerid}")
+public String editManufacturer(@Valid @ModelAttribute("manufacturer") Manufacturer manufacturer,
+                               BindingResult bindingResult,
+                               @PathVariable("manufacturerid") Long manufacturerId,
+                               Model model) {
+  
+    manufacturerRepository.save(manufacturer);
+    return "redirect:/manufacturerlist";
+}
+ 
+    
+    
+    
+    
+    
+    
+    
     @RequestMapping(value = "/deletemanufacturer/{manufacturerid}", method = RequestMethod.GET)
     public String deleteManufacturer(@PathVariable("manufacturerid") Long manufacturerid) {
         manufacturerRepository.deleteById(manufacturerid);
         return "redirect:/manufacturerlist"; // jostain syystä menee productlistiin, apuja tähän
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
